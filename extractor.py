e6a1cc0781efcc04dd0b55986a421e5a5ca55664
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2016-06-25 11:46:56 -0500

    Added call to setup and register extractor Added registration info in Docker file and moved setting up of environment variables to the top Updated wordcount extractor to reflect the changes in the pyClowder wordcount extractor

--- extractor.py
+++ extractor.py
@@ -6,13 +6,24 @@ import pyclowder.extractors as extractors
 
 
 def main():
-    global extractorName, messageType, rabbitmqExchange, rabbitmqURL    
+    global extractorName, messageType, rabbitmqExchange, rabbitmqURL, registrationEndpoints
 
     # Set logging
     logging.basicConfig(format='%(levelname)-7s : %(name)s -  %(message)s', level=logging.WARN)
     logging.getLogger('pyclowder.extractors').setLevel(logging.INFO)
+    logger = logging.getLogger('extractor')
+    logger.setLevel(logging.DEBUG)
 
-    # Connect to rabbitmq
+    # Setup
+    extractors.setup(extractorName=extractorName,
+                     messageType=messageType,
+                     rabbitmqURL=rabbitmqURL,
+                     rabbitmqExchange=rabbitmqExchange)
+
+    # Register extractor info
+    extractors.register_extractor(registrationEndpoints)
+
+    # Connect to RabbitMQ
     extractors.connect_message_bus(extractorName=extractorName, messageType=messageType,
                                    processFileFunction=process_file, rabbitmqExchange=rabbitmqExchange,
                                    rabbitmqURL=rabbitmqURL)
@@ -22,8 +33,6 @@ def main():
 # Process the file and upload the results
 def process_file(parameters):
     global extractorName
-
-    print parameters
     
     inputfile = parameters['inputfile']
 
@@ -31,12 +40,33 @@ def process_file(parameters):
     result = subprocess.check_output(['wc', inputfile], stderr=subprocess.STDOUT)
     (lines, words, characters, filename) = result.split()
 
+    # Context url
+    context_url = 'https://clowder.ncsa.illinois.edu/clowder/contexts/metadata.jsonld'
+
     # Store results as metadata
-    metadata = dict()
-    metadata["extractor_id"] = extractorName
-    metadata['lines'] = lines
-    metadata['words'] = words
-    metadata['characters'] = characters
+    metadata = \
+        {
+            '@context': [
+                context_url, {
+                    'lines': 'http://clowder.ncsa.illinois.edu/' + extractorName + '#lines',
+                    'words': 'http://clowder.ncsa.illinois.edu/' + extractorName + '#words',
+                    'characters': 'http://clowder.ncsa.illinois.edu/' + extractorName + '#characters'
+                }
+            ],
+            'attachedTo': {
+                'resourceType': 'file', 'id': parameters["fileid"]
+            },
+            'agent': {
+                '@type': 'cat:extractor',
+                'extractor_id': 'https://clowder.ncsa.illinois.edu/clowder/api/extractors/' + extractorName
+            },
+            'content': {
+                'lines': lines,
+                'words': words,
+                'characters': characters
+            }
+        }
+    print metadata
 
     # Upload metadata
     extractors.upload_file_metadata(mdata=metadata, parameters=parameters)
